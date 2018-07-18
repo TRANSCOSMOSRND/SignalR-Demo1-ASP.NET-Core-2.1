e07aa8e1428954503dbf5d9398a863613fe073e9
sho_shimoda@t-rnd.com <sho_shimoda@t-rnd.com>
2018-07-18 14:48:42 +0900

    Example Code for SignalR ASP.NET Core 2.1

---
+++ WebApplicationSignalRDemo1/Hubs/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace WebApplicationSignalRDemo1.Hubs
+{
+    /// <summary>
+    /// This is a hub class
+    /// </summary>
+    public class ChatHub : Hub
+    {
+        /// <summary>
+        /// handle sending messages
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
